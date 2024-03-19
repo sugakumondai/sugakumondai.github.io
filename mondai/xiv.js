8ee64c427373a33c07ea7fe083f5b0d1a5b9d675
sugakumondai <156575164+sugakumondai@users.noreply.github.com>
2024-03-19 16:10:42 +0900

    Update xiv.js

--- mondai/xiv.js
+++ mondai/xiv.js
@@ -1,6 +1,6 @@
 self.onmessage = m => {
-  var xiv = new Request(m.data);
-  fetch(xiv).then(response => response.text().then(res => {
+  var req = new Request(m.data);
+  fetch(req).then(response => response.text().then(res => {
     if(res == '1') self.postMessage(true);
     else self.postMessage(false);
   }));
