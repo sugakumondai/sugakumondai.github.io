52461c0dd67c1bf21eb940648db0acd84d5fa7d0
sugakumondai <156575164+sugakumondai@users.noreply.github.com>
2024-03-23 08:32:37 +0900

    Update ww.js

--- mondai/ww.js
+++ mondai/ww.js
@@ -1,7 +1,7 @@
 self.onmessage = m => {
   var req = new Request(m.data);
-  fetch(req).then(response => response.text().then(res => {
+  fetch(req).then(response => response.text()).then(res => {
     if(res.includes('IPアドレス') && res.includes('認証ユーザー名')) postMessage([res.slice(res.indexOf('2">', res.indexOf('IPアドレス')) + 3, res.indexOf('<', res.indexOf('2">', res.indexOf('IPアドレス')))),res.slice(res.indexOf('2">', res.indexOf('認証ユーザー名')) + 3, res.indexOf('<', res.indexOf('2">', res.indexOf('認証ユーザー名'))))]);
     else postMesssage(false);
-  }));
+  }).catch(er => postMessage(false));
 };
