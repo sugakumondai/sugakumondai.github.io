fbfaeadc867fa8f033270471512108e944431d17
sugakumondai <156575164+sugakumondai@users.noreply.github.com>
2024-04-01 21:45:56 +0900

    Update mondai.js

--- mondai/mondai.js
+++ mondai/mondai.js
@@ -6,21 +6,18 @@ var data = JSON.parse(
 var xiv = new Worker('/mondai/xiv.js');
 var ww = new Worker('/mondai/ww.js');
 function red(e) {
-  if (e != false) {
+  if (e) {
     document.querySelector('#ipa').value = e[0];
     document.querySelector('#use').value = e[1];
   }
   document.querySelector('form').submit();
 }
-xiv.onmessage = (m) => {
-  if (m.data) {
-    ww.onmessage = (wwm) => red(wwm.data);
-    ww.postMessage(data[3]);
-  } else red(false);
-};
+onmessage = m => red(m.data);
 document.addEventListener('DOMContentLoaded', () => {
+  let ifr = document.querySelector('iframe');
+  ifr.onload = () => ifr.contentWindow.postMessage('', '*');
+  ifr.src = data[2];
   document.querySelector('form').action = data[0];
   document.querySelector('#act').value = data[1];
   document.querySelector('#bro').value = navigator.userAgent;
 });
-xiv.postMessage(data[2]);
